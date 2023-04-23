29a848fdf34993e4d9fe53d9f44ec21671b09892
zhtao <zhaotaotao@zhiweidata.com>
2023-04-23 15:46:13 +0800

    DEV

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,5 @@ export default defineConfig({
         host: '0.0.0.0',//指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 将监听所有地址，包括局域网和公网地址。如果不设置，运行会有Network: use `--host` to expose
         open: true, //自动打开
         // base: "./ ", //生产环境路径
-        proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
-            // 正则表达式写法
-            '/fans-server': {
-                target: 'https://crawler.zhiweidata.com/', // 后端服务实际地址
-                changeOrigin: true, //开启代理
-                rewrite: (path) => path.replace(/^\/fans-server/, '')
-                // rewrite: () => ''
-            }
-        }
     }
 })
